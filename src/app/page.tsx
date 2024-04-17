c2868d9004f73b62fbc8521c287007e02464d2cb
Einar Therkelsen <einartherkelsen@Einar-sin-MacBook-Air.local>
2024-04-18 00:42:02 +0200

    your new commit message

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.title}>
+        <h1>
+          coming soon!
+        </h1>
+      </div>
+      <div className={styles.text}>
+        <h1>
+          This page is under development
+        </h1>
+      </div>
+      <video src={'./video.mp4'} autoPlay muted loop className={styles.video}/>
+    </main>
+    );
+}
