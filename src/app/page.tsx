91532e9c8a819ff14f5c52dbe6da42be5028f7f1
Einar Therkelsen <einartherkelsen@Einar-sin-MacBook-Air.local>
2025-08-10 17:16:05 +0200

    New portrait picture

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
       <ScalingSection>
         <div>
           <div className={styles.hero_container}>
-            <img src={'/images/IMG_20220810_235152.jpg'} className={styles.hero_image}/>
+            <img src={'/images/portrait.jpg'} className={styles.hero_image}/>
             <div className={styles.hero_text}>
               <h1>Einar Therkelsen</h1>
               <p>
