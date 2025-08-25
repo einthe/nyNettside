896bdb4d3300c9853029a181fa3aa0fc3bc4c59c
Einar Therkelsen <einartherkelsen@MAC-9CBB59.station>
2025-08-25 13:03:11 +0200

    new interests image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,7 +42,7 @@ export default function Home() {
       </ScalingSection> */}
 
       <div className={styles.section}>
-        <img src="/images/cyber-gløs.jpg" className={styles.image}/>
+        <img src="/images/cyber-gløs-vignette.jpg" className={styles.image}/>
       </div>
 
       <ScalingSection>
@@ -90,7 +90,7 @@ export default function Home() {
       </ScalingSection>
 
       <div className={styles.section}>
-        <img src="/images/cyber-gløs.jpg" className={styles.image}/>
+        <img src="/images/interests_vignette.jpg" className={styles.image}/>
       </div>
 
       <ScalingSection>
